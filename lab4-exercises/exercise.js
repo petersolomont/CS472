543a397baea3f06b0b8b14f4bc3e4b65b0151753
peter <petrossolomonn2003@gmail.com>
2022-05-02 15:04:59 -0500

    testf

---
+++ lab4-exercises/exercise.js
@@ -0,0 +1,10 @@
+function booyah1() {
+    alert('BOOYAH!')
+}
+let booyah2 = new function () {
+    alert('BOOYAH!')
+}
+function testQuestion3() {
+    setTimeout(booyah1, 2000);
+    setTimeout(booyah2(), 2000);
+}
